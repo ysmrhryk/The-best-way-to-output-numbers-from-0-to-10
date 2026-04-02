38ad3932dc7b8ddc3a091e3b855b286d31e1a21b
ysmrhryk <ossanbuyobuyo@gmail.com>
2026-04-02 17:36:39 +0900

    We welcome main.js.

---
+++ main.js
@@ -0,0 +1,11 @@
+var n = undefined;
+var data = '[';
+
+while(!!(JSON.stringify(data).length < parseInt('0x17', 16))){
+    n = -~n;
+    data = `${data}${n},`;
+}
+
+data = JSON.parse(`${data.substring(0, data.length-1)}]`);
+
+for(i=+[]; i<n; i=-~i) console.log(data[i]);

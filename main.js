75d0216437867da8039f64f643fe42c945d096e8
ysmrhryk <33924185+ysmrhryk@users.noreply.github.com>
2026-04-03 13:27:19 +0900

    Update main.js
    fixed bug

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var n = undefined;
+var n = 0;
 var data = '[';
 
-while(!!(data.length < parseInt('0x17', 16))){
-    n = -~n;
+while(!!(data.length <= parseInt('0x17', 16))){
     data = `${data}${n},`;
+    n = -~n;
 }
 
 data = JSON.parse(`${data.substring(0, data.length-1)}]`);
